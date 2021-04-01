2ab379858e92d80f44ad3408549a963c6081a8b2
lenalbert <lenalbert@hotmail.fr>
2021-04-01 17:11:19 +0200

    feat: add Nqueens Choco

---
+++ NQueensChoco.java
@@ -0,0 +1,27 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
+
+public class NQueensChoco {
+
+    public static void main(String[] args) {
+
+        int n=4;
+
+        Model model = new Model("NQueens");
+        IntVar[] V = model.intVarArray("V", n, 1, n);
+
+        model.allDifferent(V).post();
+        for (int i=0; i<V.length; i++){
+            for (int j=1; j<V.length-i; j++){
+                model.arithm(V[i+j], "!=", V[i], "+", j).post();
+                model.arithm(V[i+j], "!=", V[i], "-", j).post();
+            }
+        }
+
+        Solver solver = model.getSolver();
+        System.out.println(solver.findSolution());
+    
+    }
+}
