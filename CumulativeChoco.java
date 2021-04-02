78490bbd30902fb1b8ce0cd89cef29e0f1841620
lenalbert <lenalbert@hotmail.fr>
2021-04-02 14:08:07 +0200

    fix: more precise names :)

--- CumulativeChoco.java
+++ CumulativeChoco.java
@@ -13,15 +13,15 @@ public class CumulativeChoco {
         Task[] tasks = new Task[4];
         int d = 1;
         for (int i = 0; i < 4; i++) {
-            IntVar start = model.intVar("S", 1, 9);
-            IntVar duration = model.intVar("D", d++);
-            IntVar end = model.intVar("E", 1, 8);
+            IntVar start = model.intVar("S"+i, 1, 9);
+            IntVar duration = model.intVar("D"+i, d++);
+            IntVar end = model.intVar("E"+i, 1, 8);
             tasks[i] = new Task(start, duration, end);
         }
         IntVar[] heights = new IntVar[4];
         int[] val = new int[]{1, 2, 5, 7};
         for (int i = 0; i < val.length; i++) {
-            heights[i] = model.intVar("H" + i, val[i]);
+            heights[i] = model.intVar("H"+i, val[i]);
         }
         IntVar limit = model.intVar("L", 7);
 
